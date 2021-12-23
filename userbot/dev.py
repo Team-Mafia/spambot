b21f9caebb3349092aead9b44e50dcab7d3383a9
Mafia-op <96426282+Mafia-op@users.noreply.github.com>
2021-12-23 12:36:23 +0530

    list of baap

---
+++ userbot/dev.py
@@ -0,0 +1,25 @@
+DEVLIST = [
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+]
+
+
+HARMFUL = [
+    "SESSION",
+    "HEROKU_API_KEY",
+    "base64",
+    "bash",
+    "get_me()",
+    "phone",
+    "os.system",
+    "sys.stdout",
+    "sys.stderr",
+    "subprocess",
+    "USERBOT_SESSION",
+    "session.save()",
+]
