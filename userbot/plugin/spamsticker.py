871d6479e0e4502c0a1fd5d718500b15241c422d
Mafia-op <96426282+Mafia-op@users.noreply.github.com>
2021-12-23 12:02:52 +0530

    spam your sticker pack in op way

---
+++ userbot/plugin/spamsticker.py
@@ -0,0 +1,39 @@
+
+"""
+✘ Commands Available -
+
+•`{i}sspam <reply to sticker>`
+   it spam the whole stickers in that pack.
+
+"""
+
+from telethon.tl.functions.messages import GetStickerSetRequest
+from telethon.tl.types import InputStickerSetID, InputStickerSetShortName
+from telethon.utils import get_input_document
+
+from . import *
+
+
+@userbot_cmd(pattern="sspam$")
+async def _(e):
+    x = await e.get_reply_message()
+    if not (x and x.media and hasattr(x.media, "document")):
+        return await eod(e, "`Reply To Sticker Only`")
+    set = x.document.attributes[1]
+    sset = await e.client(
+        GetStickerSetRequest(
+            InputStickerSetID(
+                id=set.stickerset.id,
+                access_hash=set.stickerset.access_hash,
+            )
+        )
+    )
+    pack = sset.set.short_name
+    docs = [
+        get_input_document(x)
+        for x in (
+            await e.client(GetStickerSetRequest(InputStickerSetShortName(pack)))
+        ).documents
+    ]
+    for xx in docs:
+        await e.respond(file=(xx))
