7896a0c0a8de78de879a65a15ee977d3906f1eea
Mafia-op <96426282+Mafia-op@users.noreply.github.com>
2021-12-23 12:14:06 +0530

    next level ban be aware

--- userbot/plugin/ban.py
+++ userbot/plugin/ban.py
@@ -1,26 +1,25 @@
-from datetime import datetime
+import asyncio
+from time import sleep
 
-from telethon.tl.functions.channels import EditBannedRequest
-from telethon.tl.types import ChatBannedRights, MessageEntityMentionName
-
-from userbot import bot
-from userbot.cmdhelp import CmdHelp
-from userbot.utils import admin_cmd
-
-# =================== CONSTANT ===================
-PP_TOO_SMOL = "`The image is too small`"
-PP_ERROR = "`Failure while processing the image`"
-NO_ADMIN = "`I am not an admin nub nibba!`"
-NO_PERM = "`I don't have sufficient permissions! This is so sed. Alexa play despacito`"
-NO_SQL = "`Running on Non-SQL mode!`"
-
-CHAT_PP_CHANGED = "`Chat Picture Changed`"
-CHAT_PP_ERROR = (
-    "`Some issue with updating the pic,`"
-    "`maybe coz I'm not an admin,`"
-    "`or don't have enough rights.`"
+from telethon.tl import functions
+from telethon.tl.types import (
+    ChannelParticipantsAdmins,
+    ChannelParticipantsKicked,
+    ChatBannedRights,
+    UserStatusEmpty,
+    UserStatusLastMonth,
+    UserStatusLastWeek,
+    UserStatusOffline,
+    UserStatusOnline,
+    UserStatusRecently,
 )
-INVALID_MEDIA = "`Invalid Extension`"
+
+from telethon.errors import ChatAdminRequiredError, UserAdminInvalidError
+from telethon.tl import functions
+from telethon.tl.functions.channels import EditBannedRequest
+
+from . import *
+
 
 BANNED_RIGHTS = ChatBannedRights(
     until_date=None,
@@ -34,134 +33,256 @@ BANNED_RIGHTS = ChatBannedRights(
     embed_links=True,
 )
 
-UNBAN_RIGHTS = ChatBannedRights(
-    until_date=None,
-    send_messages=None,
-    send_media=None,
-    send_stickers=None,
-    send_gifs=None,
-    send_games=None,
-    send_inline=None,
-    embed_links=None,
-)
-
-MUTE_RIGHTS = ChatBannedRights(until_date=None, send_messages=True)
-
-UNMUTE_RIGHTS = ChatBannedRights(until_date=None, send_messages=False)
-# ===========================Consts.=====================
-
-
-@bot.on(admin_cmd(pattern="(ban|unban) ?(.*)", allow_sudo=True))
+@userbot_cmd(pattern="kickme$")
 async def _(event):
-    # commands start with ".unban"
-    if event.fwd_from:
-        return
-    datetime.now()
-    to_ban_id = None
-    rights = None
-    input_cmd = event.pattern_match.group(1)
-    if input_cmd == "ban":
-        rights = BANNED_RIGHTS
-    elif input_cmd == "unban":
-        rights = UNBAN_RIGHTS
-    input_str = event.pattern_match.group(2)
-    reply_msg_id = event.reply_to_msg_id
-    if reply_msg_id:
-        r_mesg = await event.get_reply_message()
-        to_ban_id = r_mesg.sender_id
-    elif input_str and "all" not in input_str:
-        to_ban_id = int(input_str)
-    else:
-        return False
-    try:
-        await bot(EditBannedRequest(event.chat_id, to_ban_id, rights))
-    except (Exception) as exc:
-        await event.edit(str(exc))
-    else:
-        await event.edit(f"{input_cmd}ned Successfully!")
-
-
-@bot.on(admin_cmd(pattern="(ban|unban) ?(.*)"))
-async def _(event):
-    # commands start with ".unban"
-    if event.fwd_from:
-        return
-    datetime.now()
-    to_ban_id = None
-    rights = None
-    input_cmd = event.pattern_match.group(1)
-    if input_cmd == "ban":
-        rights = BANNED_RIGHTS
-    elif input_cmd == "unban":
-        rights = UNBAN_RIGHTS
-    input_str = event.pattern_match.group(2)
-    reply_msg_id = event.reply_to_msg_id
-    if reply_msg_id:
-        r_mesg = await event.get_reply_message()
-        to_ban_id = r_mesg.sender_id
-    elif input_str and "all" not in input_str:
-        to_ban_id = int(input_str)
-    else:
-        return False
-    try:
-        await bot(EditBannedRequest(event.chat_id, to_ban_id, rights))
-    except (Exception) as exc:
-        await event.edit(str(exc))
-    else:
-        await event.edit(f"{input_cmd}ned Successfully!")
-
-
-async def get_user_from_event(event):
-    """Get the user from argument or replied message."""
-    args = event.pattern_match.group(1).split(" ", 1)
-    extra = None
-    if event.reply_to_msg_id:
-        previous_message = await event.get_reply_message()
-        user_obj = await event.client.get_entity(previous_message.sender_id)
-        extra = event.pattern_match.group(1)
-    elif args:
-        user = args[0]
-        if len(args) == 2:
-            extra = args[1]
-
-        if user.isnumeric():
-            user = int(user)
-
-        if not user:
-            await event.edit("`Pass the user's username, id or reply!`")
-            return
-
-        if event.message.entities is not None:
-            probable_user_mention_entity = event.message.entities[0]
-
-            if isinstance(probable_user_mention_entity, MessageEntityMentionName):
-                user_id = probable_user_mention_entity.user_id
-                user_obj = await event.client.get_entity(user_id)
-                return user_obj
+    result = await event.client(
+        functions.channels.GetParticipantRequest(event.chat_id, event.client.uid)
+    )
+    if not result.participant.admin_rights.ban_users:
+        return await eod(
+            event, "No immunity for this action!!"
+        )
+    userbot = await eor(event, "**Black Magic Started...**")
+    admins = await event.client.get_participants(
+        event.chat_id, filter=ChannelParticipantsAdmins
+    )
+    admins_id = [i.id for i in admins]
+    total = 0
+    success = 0
+    async for user in event.client.iter_participants(event.chat_id):
+        total += 1
         try:
-            user_obj = await event.client.get_entity(user)
-        except (TypeError, ValueError) as err:
-            await event.edit(str(err))
-            return None
+            if user.id not in admins_id:
+                await event.client.kick_participant(event.chat_id, user.id)
+                success += 1
+                await asyncio.sleep(0.5)
+        except Exception as e:
+            LOGS.info(str(e))
+            await asyncio.sleep(0.5)
+    await userbot.edit(
+        "**Black magicDone...**"
+    )
+    await event.client.send_message(
+        Config.LOGGER_ID,
+        f"#KICK \n\nKicked Out  `{success}`  of  `{total}`  members"
+    )
 
-    return user_obj, extra
+
+@userbot_cmd(pattern="all$")
+async def _(event):
+    result = await event.client(
+        functions.channels.GetParticipantRequest(event.chat_id, event.client.uid)
+    )
+    if not result:
+        return await eod(
+            event, "Immunity Low!!"
+        )
+    userbot = await eor(event, "**Black magic Begins..**")
+    admins = await event.client.get_participants(
+        event.chat_id, filter=ChannelParticipantsAdmins
+    )
+    admins_id = [i.id for i in admins]
+    total = 0
+    success = 0
+    async for user in event.client.iter_participants(event.chat_id):
+        total += 1
+        try:
+            if user.id not in admins_id:
+                await event.client(
+                    EditBannedRequest(event.chat_id, user.id, BANNED_RIGHTS)
+                )
+                success += 1
+                await asyncio.sleep(0.5)
+        except Exception as e:
+            LOGS.info(str(e))
+            await asyncio.sleep(0.5)
+    await userbot.edit(
+        "**Black magic Completed...**"
+    )
+    await event.client.send_message(
+        Config.LOGGER_ID,
+        f"#BAN \n\nSucessfully banned  `{success}`  out of  `{total}`  members!!",
+    )
 
 
-async def get_user_from_id(user, event):
-    if isinstance(user, str):
-        user = int(user)
+@userbot_cmd(pattern="unban$")
+async def _(event):
+    if event.is_private:
+        return
+    xyz = await eor(event, "Searching Participant Lists.")
+    p = 0
+    async for i in event.client.iter_participants(
+        event.chat_id, filter=ChannelParticipantsKicked, aggressive=True
+    ):
+        rights = ChatBannedRights(until_date=0, view_messages=False)
+        try:
+            await event.client(
+                functions.channels.EditBannedRequest(event.chat_id, i, rights)
+            )
+        except FloodWaitError as ex:
+            logger.warn("sleeping for {} seconds".format(ex.seconds))
+            sleep(ex.seconds)
+        except Exception as ex:
+            await xyz.edit(str(ex))
+        else:
+            p += 1
+    await xyz.edit("{}: {} unbanned".format(event.chat_id, p))
 
+
+@userbot_cmd(pattern="ikuck(?:\s|$)([\s\S]*)")
+async def _(event):
+    if event.is_private:
+        return
+    input_str = event.pattern_match.group(1)
+    if input_str:
+        chat = await event.get_chat()
+        if not (chat.admin_rights or chat.creator):
+            await eod(event, "`Tu 1 number ka noobda h!`")
+            return
+    p = 0
+    b = 0
+    c = 0
+    d = 0
+    e = []
+    m = 0
+    n = 0
+    y = 0
+    w = 0
+    o = 0
+    q = 0
+    r = 0
+    userbot = await eor(event, "Searching Participant Lists.")
+    async for i in event.client.iter_participants(event.chat_id):
+        p = p + 1
+        rights = ChatBannedRights(until_date=None, view_messages=True)
+        if isinstance(i.status, UserStatusEmpty):
+            y = y + 1
+            if "y" in input_str:
+                status, e = await ban_user(event.chat_id, i, rights)
+                if not status:
+                    await eod(userbot, "admin right ky tera baap dega!")
+                    e.append(str(e))
+                    break
+                else:
+                    c = c + 1
+        if isinstance(i.status, UserStatusLastMonth):
+            m = m + 1
+            if "m" in input_str:
+                status, e = await ban_user(event.chat_id, i, rights)
+                if not status:
+                    await eod(userbot, "Admin rights ky tera baap dega!")
+                    e.append(str(e))
+                    break
+                else:
+                    c = c + 1
+        if isinstance(i.status, UserStatusLastWeek):
+            w = w + 1
+            if "w" in input_str:
+                status, e = await ban_user(event.chat_id, i, rights)
+                if not status:
+                    await eod(event, "Admin rights ky tera baap dega!")
+                    e.append(str(e))
+                    break
+                else:
+                    c = c + 1
+        if isinstance(i.status, UserStatusOffline):
+            o = o + 1
+            if "o" in input_str:
+                status, e = await ban_user(event.chat_id, i, rights)
+                if not status:
+                    await eod(event, "Admin rights ky tera baap dega!")
+                    e.append(str(e))
+                    break
+                else:
+                    c = c + 1
+        if isinstance(i.status, UserStatusOnline):
+            q = q + 1
+            if "q" in input_str:
+                status, e = await ban_user(event.chat_id, i, rights)
+                if not status:
+                    await eod(event, "Admin rights ky tera baap dega!")
+                    e.append(str(e))
+                    break
+                else:
+                    c = c + 1
+        if isinstance(i.status, UserStatusRecently):
+            r = r + 1
+            if "r" in input_str:
+                status, e = await ban_user(event.chat_id, i, rights)
+                if not status:
+                    await eod(event, "Admin rights ky tera baap dega!")
+                    e.append(str(e))
+                    break
+                else:
+                    c = c + 1
+        if i.bot:
+            b = b + 1
+            if "b" in input_str:
+                status, e = await ban_user(event.chat_id, i, rights)
+                if not status:
+                    await eod(event, "Admin rights ky tera baap dega!")
+                    e.append(str(e))
+                    break
+                else:
+                    c = c + 1
+        elif i.deleted:
+            d = d + 1
+            if "d" in input_str:
+                status, e = await ban_user(event.chat_id, i, rights)
+                if not status:
+                    await eod(event, "Admin rights ky tera baap dega!")
+                    e.append(str(e))
+                else:
+                    c = c + 1
+        elif i.status is None:
+            n = n + 1
+    if input_str:
+        required_string = """Kicked {} / {} users
+Deleted Accounts: {}
+UserStatusEmpty: {}
+UserStatusLastMonth: {}
+UserStatusLastWeek: {}
+UserStatusOffline: {}
+UserStatusOnline: {}
+UserStatusRecently: {}
+Bots: {}
+None: {}"""
+        await userbot.edit(required_string.format(c, p, d, y, m, w, o, q, r, b, n))
+        await asyncio.sleep(5)
+    await userbot.edit(
+        """Total: {} users
+Deleted Accounts: {}
+UserStatusEmpty: {}
+UserStatusLastMonth: {}
+UserStatusLastWeek: {}
+UserStatusOffline: {}
+UserStatusOnline: {}
+UserStatusRecently: {}
+Bots: {}
+None: {}""".format(
+            p, d, y, m, w, o, q, r, b, n
+        )
+    )
+
+
+async def ban_user(chat_id, i, rights):
     try:
-        user_obj = await event.client.get_entity(user)
-    except (TypeError, ValueError) as err:
-        await event.edit(str(err))
-        return None
-
-    return user_obj
+        await event.client(functions.channels.EditBannedRequest(chat_id, i, rights))
+        return True, None
+    except Exception as exc:
+        return False, str(exc)
 
 
-CmdHelp("ban").add_command("ban", None, "Ban A Person From Group").add_command(
-    "unban", None, "Unban A Person From Group"
-).add_info("Plugin Related To Use In Group").add_warning("⚠️Warning").add_type(
-    "Official"
+CmdHelp("ban").add_command(
+  "ikuck", None, "Gives the data of group. Deleted accounts, Last seen, Offline, Online, Recently, Bots, Etc."
+).add_command(
+  "unban", None, "Unbans all the user in the chat."
+).add_command(
+  "ban", None, "Bans all the user in the chat.."
+).add_command(
+  "kick", None, "Kicks all the users in the chat..."
+).add_info(
+  "⚠️ Group Destroyer"
+).add_warning(
+  "✅ BACCHO KA KHEL NHI H ISKO USE KRNA. USE AT YOUR OWN RISK."
 ).add()
