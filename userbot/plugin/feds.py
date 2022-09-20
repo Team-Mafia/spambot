6e4fade5eee97380345308cf45535cb531c16367
UNKNOWN <79321850+Jatindalal875@users.noreply.github.com>
2022-09-20 19:31:01 +0530

    Update feds.py

--- userbot/plugin/feds.py
+++ userbot/plugin/feds.py
@@ -5,7 +5,9 @@ from telethon.errors.rpcerrorlist import YouBlockedUserError
 from telethon.tl.functions.users import GetFullUserRequest
 
 from ..utils import admin_cmd , sudo_cmd
+from . import ALIVE_NAME
 
+naam = str(ALIVE_NAME)
 
 bots = "@MissRose_bot"
 
